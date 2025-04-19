15e440b97dcb75d437c0d831005411426eebbaa8
jecDSC <jeremycho521@gmail.com>
2025-04-18 20:07:10 -0700

    fixed navbar

--- global.js
+++ global.js
@@ -22,14 +22,14 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/";
+
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
-
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/website/";         // GitHub Pages repo name
-  url = !url.startsWith('http') ? BASE_PATH + url : url;
 
